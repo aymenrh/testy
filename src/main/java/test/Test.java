a41868f00b0f1183c3fed5fd59845505970b9884
aymen <aymen.rahmouni@esprit.tn>
2016-07-24 01:03:13 +0100

    test pour le git

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,28 @@
+package test;
+
+public class Test {
+
+	private Integer id;
+	private String nom;
+	
+	public Test(Integer id,String nom)
+	{
+		
+		this.id = id;
+		this.nom = nom;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+}
